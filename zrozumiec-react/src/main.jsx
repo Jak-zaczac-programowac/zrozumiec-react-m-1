c36d6bcae14496297cec760c3b267a4b7502f7f8
filipMaM <filip.mamcarczyk@gmail.com>
2023-07-28 11:30:11 +0200

    Lesson 5 start

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -1,37 +1,5 @@
 import ReactDOM from "react-dom/client";
 
-const stringContent = "Test 1!";
-const numberContent = 13;
-const calcContent = 10 + 5;
-function getRandomNumber() {
-    console.log(Math.round(Math.random() * 100));
-}
-const person = {
-    name: "Filip",
-    age: 29,
-};
-const paragraphElement = (
-    <>
-        <h2>Heading</h2>
-        <a href="http://zrozumiecReact.pl" className="link" for="">
-            Strona kursu
-        </a>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-            numquam, corporis nam voluptate libero quas. Placeat ipsum unde
-            quidem voluptatem.
-        </p>
-    </>
-);
-const element = (
-    <div>
-        {paragraphElement}
-        {paragraphElement}
-        {paragraphElement}
-        {paragraphElement}
-        {paragraphElement}
-        {paragraphElement}
-    </div>
-);
+const element = <div></div>;
 
 ReactDOM.createRoot(document.getElementById("root")).render(element);
