118bbf15cd325c4f617d3e58c4757f8266e06c53
filipMaM <filip.mamcarczyk@gmail.com>
2023-07-28 11:29:31 +0200

    Lesson 4 end

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -1,21 +1,37 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = React.createElement(
-    "a",
-    { href: "https://zrozumiecReact.pl" },
-    "test2"
+const stringContent = "Test 1!";
+const numberContent = 13;
+const calcContent = 10 + 5;
+function getRandomNumber() {
+    console.log(Math.round(Math.random() * 100));
+}
+const person = {
+    name: "Filip",
+    age: 29,
+};
+const paragraphElement = (
+    <>
+        <h2>Heading</h2>
+        <a href="http://zrozumiecReact.pl" className="link" for="">
+            Strona kursu
+        </a>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+            numquam, corporis nam voluptate libero quas. Placeat ipsum unde
+            quidem voluptatem.
+        </p>
+    </>
 );
-const elementInJSX = <a href="https://zrozumiecReact.pl">test 3</a>;
-const currentDate = new Date();
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-console.log(hours, minutes);
-
-const timer = (
+const element = (
     <div>
-        {hours}:{minutes}
+        {paragraphElement}
+        {paragraphElement}
+        {paragraphElement}
+        {paragraphElement}
+        {paragraphElement}
+        {paragraphElement}
     </div>
 );
 
-ReactDOM.createRoot(document.getElementById("root")).render(timer);
+ReactDOM.createRoot(document.getElementById("root")).render(element);
