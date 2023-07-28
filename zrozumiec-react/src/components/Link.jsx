0f68fc9ff1e06630e444d7a6938924e975a4faa9
filipMaM <filip.mamcarczyk@gmail.com>
2023-07-28 13:12:58 +0200

    Lesson 5 finish

---
+++ zrozumiec-react/src/components/Link.jsx
@@ -0,0 +1,8 @@
+export function Link() {
+    const address = "https://zrozumiecreact.pl";
+    return (
+        <a href={address} target="_blank" rel="noopener noreferrer">
+            Sprawdź stronę teraz!
+        </a>
+    );
+}
--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -1,5 +1,19 @@
 import ReactDOM from "react-dom/client";
+import { Link } from "./components/Link";
 
-const element = <div></div>;
+const element = (
+    <>
+        <h1>Nasza strona!</h1>
+        <main>
+            <Link />
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+                labore blanditiis magnam eveniet rem ratione, voluptatum nulla
+                inventore quaerat necessitatibus.
+            </p>
+            <Link />
+        </main>
+    </>
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(element);
