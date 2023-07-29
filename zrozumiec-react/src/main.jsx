f6fcc42bb1d239c766ff68c31d98a7c4c4413684
filipMaM <filip.mamcarczyk@gmail.com>
2023-07-29 12:37:48 +0200

    Lesson 7 end

--- zrozumiec-react/src/main.jsx
+++ zrozumiec-react/src/main.jsx
@@ -1,4 +1,24 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { Link } from "./components/Link";
+import { HeadingItalic } from "./components/HeadingItalic";
+import { Logger } from "./components/Logger";
+const element = (
+    <>
+        <Logger>
+            <HeadingItalic>Nasza super strona</HeadingItalic>
+        </Logger>
+        <main>
+            <Logger>
+                <Link text="Sprawdź stronę kursu!" shouldOpenNewTab={true} />
+            </Logger>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+                labore blanditiis magnam eveniet rem ratione, voluptatum nulla
+                inventore quaerat necessitatibus.
+            </p>
+            <Link shouldOpenNewTab={false} />
+        </main>
+    </>
+);
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(element);
